dacff712db0eef303ff215d3b6bfa1c70bd2f7d2
Jeel Gajera <jeelgajera200@gmail.com>
2024-01-30 13:32:51 +0530

    feat - common words, emoji analysis

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pre_processor, helper
+import plotly.express as px
 
 # create title with  
 st.sidebar.image("https://www.freepnglogos.com/uploads/whatsapp-logo-light-green-png-0.png", width=150)
@@ -25,6 +26,8 @@ if uploaded_file is not None:
     selected_user = st.sidebar.selectbox("Show Analysis wrt", user_list)
 
     if st.sidebar.button("Show Analysis"):
+
+        # Stats Area
         num_msg, num_words, total_media, total_links = helper.fetch_stats(selected_user, df)
         c1, c2, c3, c4 = st.columns(4)
 
@@ -43,3 +46,38 @@ if uploaded_file is not None:
         with c4:
             st.header("Total Links")
             st.title(total_links)
+
+        # Top Users Activity
+        if selected_user == 'Overall':
+            st.title("Users Activity")
+            c1, c2 = st.columns(2)
+
+            with c1:
+                st.header("Most Busy Users")
+                x, per_df = helper.most_busy_users(df)
+                fig = px.bar(x, x=x.values, y=x.index, orientation='h', )
+                st.plotly_chart(fig)
+
+            with c2:
+                st.header("Individual Contribution")
+                st.dataframe(per_df)
+
+        # Word Cloud
+        st.title("Word Cloud")
+        word_cloud = helper.create_wordcloud(selected_user, df)
+        st.image(word_cloud.to_image())
+
+        # Most Common Words
+        st.title("Most Common Words")
+        most_common_words = helper.most_common_words(selected_user, df)
+        fig = px.bar(most_common_words, x='word', y='count')
+        st.plotly_chart(fig)
+
+        # Most Common Emojis
+        st.title("Most Common Emojis")
+        emoji_df = helper.most_common_emojis(selected_user, df)
+        # create pie chart 
+        fig = px.pie(emoji_df, values='count', names='emoji')
+        st.plotly_chart(fig)
+
+
--- helper.py
+++ helper.py
@@ -1,5 +1,8 @@
 import pandas as pd
+from collections import Counter
+from wordcloud import WordCloud
 from urlextract import URLExtract
+import emoji 
 extract = URLExtract()
 
 def fetch_stats(selected_user, df):
@@ -19,3 +22,59 @@ def fetch_stats(selected_user, df):
     total_links = len(links)
 
     return total_msgs, total_words, total_media, total_links
+
+
+def most_busy_users(df):
+    # most busy users
+    x = df['user'].value_counts().head()
+    # individual persantage of messages
+    per_df = round((df['user'].value_counts()/df.shape[0])*100, 2).reset_index().rename(columns={'count': 'percentage', 'user': 'name'})
+    return x, per_df
+
+def create_wordcloud(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df.user == selected_user]
+
+    wc = WordCloud(width=500, height=300, min_font_size=10, background_color='white')
+    # remove media ommited msg
+    df = df[df.message != '<Media omitted>\n']
+    #  remove group notification
+    df = df[df.user != 'group_notification']
+    wc.generate(' '.join(df['message']))
+    wc_df = wc.generate(df['message'].str.cat(sep=' '))
+    return wc_df
+
+def most_common_words(selected_user, df):
+    f = open('stop_words.txt', 'r', encoding='utf-8')
+    stop_words = f.read().split('\n')
+    if selected_user != 'Overall':
+        df = df[df.user == selected_user]
+
+    # remove media ommited msg
+    df = df[df.message != '<Media omitted>\n']
+    #  remove group notification
+    df = df[df.user != 'group_notification']
+    # remove stop words
+    words = []
+    for msg in df['message']:
+        for word in msg.split():
+            if word.lower() not in stop_words:
+                words.append(word.lower())
+
+    top_20 = Counter(words).most_common(20)
+    top_20 = pd.DataFrame(top_20).rename(columns={0: 'word', 1: 'count'})
+    return top_20
+
+def most_common_emojis(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df.user == selected_user]
+
+    emojis = []
+    for msg in df['message']:
+        for char in msg:
+            if emoji.is_emoji(char):
+                emojis.append(char)
+
+    top_20 = Counter(emojis).most_common(20)
+    top_20 = pd.DataFrame(top_20).rename(columns={0: 'emoji', 1: 'count'})
+    return top_20
